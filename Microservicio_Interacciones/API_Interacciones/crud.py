cb971162b1b0c5bf78edcf858b7349b3b3029ff6
jaime2302 <jmasapas@alumnos.unex.es>
2024-12-22 20:47:49 +0100

    STREAM-130-renombrar-el-parametro-idcontenido-para-que-coin

--- Microservicio_Interacciones/API_Interacciones/crud.py
+++ Microservicio_Interacciones/API_Interacciones/crud.py
@@ -115,8 +115,8 @@ def mostrar_me_gusta(db: Session, usuario_id: str):
     return me_gusta
 
 #Función para dar "Me Gusta" a un contenido por un usuario
-def dar_me_gusta(db: Session, idUsuario: str, idContenido: str):
-    tupla_lista = models.ListaMeGusta(idUsuario=idUsuario,
+def dar_me_gusta(db: Session, id_usuario: str, idContenido: str):
+    tupla_lista = models.ListaMeGusta(idUsuario=id_usuario,
                                       idContenido=idContenido)
     db.add(tupla_lista)
     db.commit()
